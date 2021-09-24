da3328a43cb4fe26ed2eedab37c7e457f7eab2b3
abhishek-pokhriyal <abhishek-pokhriyal@users.noreply.github.com>
2021-09-24 20:25:44 +0000

    Fix styling

--- src/AuditService.php
+++ src/AuditService.php
@@ -1,28 +1,29 @@
 <?php
 
 namespace ColoredCow\LaravelAudit;
+
 use ColoredCow\LaravelAudit\Events\Auditable;
 use ColoredCow\LaravelAudit\Models\ApplicationActivityAudit;
 
-class AuditService 
+class AuditService
 {
-   use Auditable;
-    
-   public function make($attr)
+    use Auditable;
+
+    public function make($attr)
     {
-      $defaultValues = [
+        $defaultValues = [
          'action' => 'No Action defined',
          'ip' => request()->ip(),
          'referer' => request()->headers->get('referer'),
          'user_id' => \Auth::id(),
-         'description' => null
+         'description' => null,
       ];
-      $attr =  array_merge($defaultValues, $attr);
-      $audit['action'] =  $attr['action'] ?? null ;
-      $audit['ip'] =      $attr['ip'] ?? request()->ip() ;
-      $audit['referer'] = $attr['referer'] ?? null ;
-      $audit['user_id'] = $attr['user_id'] ?? \Auth::id() ;
-      $audit['description'] = $attr['description'] ?? null ;
-      ApplicationActivityAudit::create($audit);
+        $attr = array_merge($defaultValues, $attr);
+        $audit['action'] = $attr['action'] ?? null ;
+        $audit['ip'] = $attr['ip'] ?? request()->ip() ;
+        $audit['referer'] = $attr['referer'] ?? null ;
+        $audit['user_id'] = $attr['user_id'] ?? \Auth::id() ;
+        $audit['description'] = $attr['description'] ?? null ;
+        ApplicationActivityAudit::create($audit);
     }
 }
